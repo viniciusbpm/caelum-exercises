d71e660f5c343a84c0d72ecc7c8353ac31192e50
Vinícius <viniciusmpacheco22@gmail.com>
2024-03-05 09:05:49 -0300

    adding exercises from chapter 10

---
+++ ed/src/main/java/br/com/caelum/ed/mapas/Carro.java
@@ -0,0 +1,45 @@
+package br.com.caelum.ed.mapas;
+
+public class Carro {
+    private String nome;
+    private String marca;
+    private String cor;
+    private int ano;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    @Override
+    public String toString() {
+        return "Carro: " + this.nome;
+    }
+}
